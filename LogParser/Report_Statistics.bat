32be6dab01cd52200358b2019b766bc3dbd5f0c6
Vadzim Zianiuk <Vadzim_Zianiuk@epam.com>
2021-06-15 16:36:15 +0300

    fix reports

--- LogParser/Report_Statistics.bat
+++ LogParser/Report_Statistics.bat
@@ -1,4 +1,4 @@
 @echo off
-set logPath= ..\BrainstormSessions\bin\Debug\netcoreapp3.0\Logs
-LOGPARSER "Select substr(text, 24, 5) AS Level, Count(1) AS Count from %logPath%\*.log GROUP BY Level" -i:TEXTLINE -o:NAT
+set logPath= ..\BrainstormSessions\bin\Debug\netcoreapp3.0\Logs\*.log
+LOGPARSER "Select substr(text, 24, 5) AS Level, Count(1) AS Count from %logPath% GROUP BY Level" -i:TEXTLINE -o:NAT
 pause;
